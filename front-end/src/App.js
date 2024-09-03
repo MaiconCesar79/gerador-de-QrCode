61651cd0e2f51fd6836328d4f7afe707ec6c73ca
Maicon Cesar <maiconcesarx@hotmail.com>
2024-09-02 23:06:08 -0300

    feat: QrCode e botão de download

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
-import {Input, Row, Col} from 'reactstrap';
+import {Input, Button} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import QRCode from "react-qr-code";
+import QRCodeLink from 'qrcode';
 
 function App() {
 
-  const[text, setText] = useState("");
+  const[linkText, setLinkText] = useState("");
+  const[linkQrCode, setLinkQrCode] = useState('');
+
+  function handleQrCode(e){
+    const value = e.target.value;
+    setLinkText(value);
+    handleGenerate(value);
+  }
+
+  function handleGenerate(link_url){
+    QRCodeLink.toDataURL(link_url,{
+      width: 600,
+      margin: 3
+    }, function(err, url){
+      setLinkQrCode(url);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <Row>
-        <Col className="bg-light border">
-          <Input className='link' onChange={(element) => {setText(element.target.value)}} value={text} 
-          />
-        </Col>
-      </Row>
-      
-      </header>
+    <div className="app"> 
+      <QRCode className='Qr' value={linkText}/>
+      <Input className='link' onChange={(e) => {handleQrCode(e)}} value={linkText} 
+      />
+      <br/>
+      <Button color="primary" href={linkQrCode} download={`qrcode.png`}>
+        Salvar QRCode
+      </Button>
     </div>
   );
 }
